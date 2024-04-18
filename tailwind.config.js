97dc57f9e23368e3bdf66637db2a7c33271630a4
Huỳnh Ngọc Bảo <Lawrence-Miyato>
2024-04-18 18:48:47 +0700

    Do_An

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./website/**/Index.{html,js}"],
+  content: ["./website/src/**/Index.{html,js}"],
   theme: {
     extend: {},
   },
